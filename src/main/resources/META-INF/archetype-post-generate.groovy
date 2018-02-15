e630bb1acdd3d72c0334a37f8c7399e8620fd5b6
Josh Reagan <jreagan@redhat.com>
2018-02-14 19:11:37 -0700

    initial commit

---
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,73 @@
+import java.io.File
+import java.net.URL
+import java.nio.file.Paths
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+
+
+println "Downloading Swagger API from ${request.properties['swaggerApiUrl']}..."
+
+def url = new URL(request.properties['swaggerApiUrl'])
+def apiText = url.text
+
+
+
+println "Writing Swagger API definition to swagger-api.json file..."
+
+def fileDir = Paths.get(request.outputDirectory, request.artifactId, 'src/main/swagger').toFile()
+def fileName = 'swagger-api.json'
+def file = new File(fileDir, fileName)
+fileDir.mkdirs();
+file.setText(JsonOutput.prettyPrint(apiText))
+
+
+
+println "Parsing Swagger API definition..."
+
+def jsonSlurper = new JsonSlurper()
+def apiMap = jsonSlurper.parseText(apiText)
+
+def apiLicense = apiMap?.info?.license?.name
+def apiLicenseUrl = apiMap?.info?.license?.url
+def apiTitle = apiMap?.info?.title
+def apiTermsOfServiceUrl = apiMap?.info?.termsOfService
+def apiVersion = apiMap?.info?.version
+def apiDescription = apiMap?.info?.description
+def apiContact = apiMap?.info?.contact?.email
+
+def apiNames = apiMap?.tags*.name
+
+
+
+println "Modifying application.yml file..."
+
+def quoteYamlString(def yamlString) {
+  return (yamlString) ? "'${yamlString.replaceAll('\'', '\'\'')}'" : null
+}
+
+def applicationYmlFile = Paths.get(request.outputDirectory, request.artifactId, 'src/main/resources', 'application.yml').toFile()
+def applicationYamlText = applicationYmlFile.text
+applicationYamlText = applicationYamlText.replaceAll('__CAMEL_SPRINGBOOT_NAME__', request.artifactId.split('-').collect() { it.capitalize() }.join(''))
+applicationYamlText = applicationYamlText.replaceAll('__API_LICENSE__', quoteYamlString(apiLicense)?:'')
+applicationYamlText = applicationYamlText.replaceAll('__API_LICENSE_URL__', quoteYamlString(apiLicenseUrl)?:'')
+applicationYamlText = applicationYamlText.replaceAll('__API_TITLE__', quoteYamlString(apiTitle)?:'')
+applicationYamlText = applicationYamlText.replaceAll('__API_TERMS_OF_SERVICE_URL__', quoteYamlString(apiTermsOfServiceUrl)?:'')
+applicationYamlText = applicationYamlText.replaceAll('__API_VERSION__', quoteYamlString(apiVersion)?:'')
+applicationYamlText = applicationYamlText.replaceAll('__API_DESCRIPTION__', quoteYamlString(apiDescription)?:'')
+applicationYamlText = applicationYamlText.replaceAll('__API_CONTACT__', quoteYamlString(apiContact)?:'')
+applicationYmlFile.setText(applicationYamlText)
+
+
+
+println "Modifying CamelConfiguration.java file..."
+
+def apiClassNames = apiNames.collect() { name ->
+  name = "${request.properties['package']}.api.${name.capitalize()}Api"
+}
+def camelConfigurationJavaFile = Paths.get(request.outputDirectory, request.artifactId, 'src/main/java', request.properties['package'].replaceAll('\\.', File.separator), 'CamelConfiguration.java').toFile()
+def camelConfigurationJavaText = camelConfigurationJavaFile.text
+camelConfigurationJavaText = camelConfigurationJavaText.replaceAll('__API_NAME__', apiTitle?:'Application')
+camelConfigurationJavaText = camelConfigurationJavaText.replaceAll('__API_CLASSES__', apiClassNames.join(','))
+camelConfigurationJavaFile.setText(camelConfigurationJavaText)
---
+++ src/main/resources/archetype-resources/src/main/java/CamelConfiguration.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ${package};
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelConfiguration extends RouteBuilder {
+
+  private static final Logger log = LoggerFactory.getLogger(CamelConfiguration.class);
+  
+  @Autowired
+  private ApiProperties apiProperties;
+  
+  @Override
+  public void configure() throws Exception {
+
+    fromF("cxfrs:%s?bindingStyle=SimpleConsumer&resourceClasses=__API_CLASSES__&features=#cxfFeatures&providers=#cxfProviders", apiProperties.getBasePath())
+      .log(LoggingLevel.INFO, log, "__API_NAME__ API invoked. Please add implementation...")
+    ;
+  }
+}
